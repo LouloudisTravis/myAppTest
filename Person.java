8c922635f102edbbe5d1438ebd4980b378e8dd17
Travis Louloudis <travislouloudis@gmail.com>
2019-01-09 21:32:33 -0500

    New Class Person

---
+++ Person.java
@@ -0,0 +1,8 @@
+public class Person{
+	private String firstName;
+	private String lastName;
+
+	public Person(String firstName, String lastName){
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+}
